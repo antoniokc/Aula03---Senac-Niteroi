60e971ffd84c46d3d6a769e22f460f32dea693b2
antoniokc <antoniok123n@gmail.com>
2025-04-10 21:20:51 -0300

    inicia o repositorio

---
+++ aula03/Atividade01.py
@@ -0,0 +1,12 @@
+
+# Entrada
+quantidadevenda = int(input("quantas camisetas o senhor comprou? "))
+
+# Processamento
+desconto = 83 * 0.13
+preço = 83
+aplicação = (preço - desconto)
+vendatotal = (quantidadevenda * aplicação)
+
+# Saida
+print('O desconto da camiseta vai ser ', desconto, ". Já o valor da camiseta será ", aplicação, ", e agora sua venda deu um valor total de",vendatotal, ",vai ser no Credito ou Debito?")
